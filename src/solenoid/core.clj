0b7e34bfbecc7406cb6ec41b0dd1300f7003baf9
Adam James <adam.vermeer2@gmail.com>
2023-01-16 13:59:17 -0800

    Add core.clj, will need to put in the API

---
+++ src/solenoid/core.clj
@@ -0,0 +1,5 @@
+(ns solenoid.core
+  (:require [solenoid.controls :as controls]
+            [solenoid.server :as server]))
+
+;; TOOD: make an API here that will automatically spawn a server the first time you use letcontrols.
